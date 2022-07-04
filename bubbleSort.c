6b895e5ae091a93233351ac422c9a9d9501a13b2
Pankaj-103 <84080470+Pankaj-103@users.noreply.github.com>
2022-07-04 22:31:15 +0530

    Create bubbleSort.c

---
+++ bubbleSort.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void swap(int* xp, int* yp)
+{
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+
+void bubbleSort(int arr[], int n)
+{
+    int i, j;
+    for (i = 0; i < n - 1; i++)
+    {
+
+       
+       
+        for (j = 0; j < n - i - 1; j++){
+			if (arr[j] > arr[j + 1])
+				swap(&arr[j], &arr[j + 1]);
+        }
+    
+    }
+
+}
+
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i = 0; i < size; i++)
+    {
+        printf("%d\t", arr[i]);
+    }
+}
+
+int main()
+{
+
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    bubbleSort(arr, n);
+    printf("sorted array:\n");
+    printArray(arr, n);
+
+    return 0;
+}
